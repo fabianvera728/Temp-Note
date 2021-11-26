2f9145555a926d549ca0b1eb4739c4d33e95f9a0
Fabian VC <fabianvera728@gmail.com>
2021-11-26 18:46:24 -0500

    [feat] features for create new note

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Router } from "./core/main/router/Router";
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
--- src/core/presentation/ui/pages/notes/NotesComponent.tsx
+++ src/core/presentation/ui/pages/notes/NotesComponent.tsx
@@ -4,22 +4,35 @@ import Note from "./components/note/note";
 import "./notes.css";
 import useFilter from "../../../../../modules/core/hooks/filter";
 import NOTES from "../../../../../modules/core/mocks/notes";
+import NewNote from "../../components/NewNote";
+import {useState} from "react";
 
 const Notes = () => {
-    const notes = NOTES;
+    const [notes, setNotes] = useState(NOTES);
     const { query, setQuery, results } = useFilter(notes);
+    const [active, setActive] = useState<boolean>(false);
+
+    const createNote = (note: INote) => {
+        setNotes([...notes, note])
+    }
 
     return (
         <div className="Notes">
             <h3>
                 <strong>Notes</strong>
             </h3>
-            <input
-                value={query}
-                type="text"
-                onChange={(e: any) => setQuery(e.target.value)}
-            />
-            <div className="Notes__notes">
+            <NewNote active={active} createNote={createNote} setActive={setActive}/>
+            <div className="is-flex is-flex-direction-row">
+                <button className="button is-primary" onClick={() => setActive(true)}>New note</button>
+                <input
+                    className="input"
+                    value={query}
+                    placeholder="Search note"
+                    type="text"
+                    onChange={(e: any) => setQuery(e.target.value)}
+                />
+            </div>
+            <div className="Notes__notes pt-4">
                 {results.map((note: INote) => (
                     <Note key={shortid.generate()} {...note} />
                 ))}
